6d75edc2307304e75c39976e5be95e71eaab5f4f
Winrey <i@iaside.com>
2021-12-19 20:22:15 +0800

    feat: Add a new  string validator

---
+++ src/validators/string.ts
@@ -0,0 +1,7 @@
+import { Validator } from "./validator";
+
+export class IsStringValidator extends Validator<object> {
+  async doValidate(value: object | unknown): Promise<boolean> {
+    return typeof value === 'string';
+  }
+}
